599431098d10352540df94f77e3ce03ce7d2d7c8
voltamonkey <9461@holbertonstudents.com>
2024-10-28 15:04:04 -0400

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program that prints digit number of base 10 starting from 0
+ *
+ * Return: Always  0.
+ */
+int main(void)
+{
+
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		printf("%d", m);
+	}
+		printf("\n");	
+
+		return (0);
+}
