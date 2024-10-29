5a1c42982747fefc415524e04f7d787f9d1047a7
voltamonkey <9461@holbertonstudents.com>
2024-10-29 15:51:37 -0400

    Typo fix Directory

---
+++ hello_world/io_demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char f,m,l;
+	int age;
+
+	scanf ("%c %c %d", &f, &m, &age);	
+	printf ("My initials are: %c%C%C and my age is %d. \n ", 'J', 'R', 'E',28);
+       return (0);	
+}
