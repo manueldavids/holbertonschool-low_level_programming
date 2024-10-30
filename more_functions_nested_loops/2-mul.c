69728b9f98b56da754fea82bc197590f1c3c970b
voltamonkey <9461@holbertonstudents.com>
2024-10-30 11:49:28 -0400

    Mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - used to multiply the ints
+ *
+ * @a: first int multiplied
+ *
+ * @b: second ind multiplied
+ *
+ * Return: void
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
