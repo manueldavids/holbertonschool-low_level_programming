d1b45b5467c57354c2d179ec058e24c627ee8843
voltamonkey <9461@holbertonstudents.com>
2024-10-31 09:39:36 -0400

    ABS

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,3 +1,20 @@
 #include "main.h"
 /**
+ * abs - computes absolute values 
  *
+ * @r: The integer to compute the absolute value of
+ *
+ * Return: Absoulte value of r.
+ */
+int _abs(int r)
+{
+
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+}
