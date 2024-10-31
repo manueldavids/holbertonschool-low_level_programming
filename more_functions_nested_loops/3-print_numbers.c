d8994739d0ab4a0184ac05532fbf248edcffdc63
voltamonkey <9461@holbertonstudents.com>
2024-10-31 08:04:59 -0400

    3-print_numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; ++x)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
