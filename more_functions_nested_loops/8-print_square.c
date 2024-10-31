fc4ba144185307883930f4492a5b3cb9c36dddfa
voltamonkey <9461@holbertonstudents.com>
2024-10-31 08:42:06 -0400

    print_square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - Prints a square.
+ *
+ * @size: int used to check
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int a,b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; ++a)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+
+			if (a == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
