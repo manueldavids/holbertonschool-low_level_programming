4a4ef9d7d4453253f9dfbc45179ef2c32117119a
voltamonkey <9461@holbertonstudents.com>
2024-10-31 08:29:10 -0400

    7-print_diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a straight line in the terminal.
+ *
+ * @n: int used to check
+ *
+ */
+void print_line(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (b == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
