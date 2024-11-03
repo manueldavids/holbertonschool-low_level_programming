4f5555d91b77ea2a6944654ec21f2eeb7137f870
voltamonkey <9461@holbertonstudents.com>
2024-11-03 17:24:07 -0400

    AddAll

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,19 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 int _putchar(char c);
+
+char *leet(char *);
+
+char *cap_string(char *);
+
+char *string_toupper(char *);
+
+void reverse_array(int *a, int n);
+
+int _strcmp(char *s1, char *s2);
+
+char *_strncpy(char *dest, char *src, int n);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strcat(char *dest, char *src);
