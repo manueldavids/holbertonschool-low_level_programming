ec7e245db21d670df3f9406c2a25c9dbe487cf11
voltamonkey <9461@holbertonstudents.com>
2024-11-08 12:20:00 -0400

    print_rev_recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,10 +1,8 @@
 #include "main.h"
-/*
- * _print_rev_recursion - Prints a string
- * @s: char
- * return: 0
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to be printed in revesed
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
