870b75f292aa25e4538ac65f58e845658e0fffe6
voltamonkey <9461@holbertonstudents.com>
2024-12-06 16:33:33 -0400

    main.cupdate

--- variadic_functions/2-main.c
+++ variadic_functions/2-main.c
@@ -1,12 +1,20 @@
 #include "variadic_functions.h"
 
 /**
- * main - check the code
+ * main - Entry point to test the print_strings function.
  *
- * Return: Always 0.
+ * Description: This program demonstrates the use of the `print_strings`
+ * function by passing a variable number of strings along with a separator
+ * to print them in a formatted way.
+ *
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-    print_strings(", ", 2, "Jay", "Django");
-    return (0);
+	/* Call print_strings with a separator, count, and strings */
+	print_strings(", ", 2, "Jay", "Django");
+
+	/* Indicate successful completion of the program */
+	return (0);
 }
+
