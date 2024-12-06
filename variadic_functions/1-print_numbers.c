860a63ecfbb3ee99ebd5be570ee0eacc9e47de55
voltamonkey <9461@holbertonstudents.com>
2024-12-06 16:25:42 -0400

    updateprint_num

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -5,25 +5,40 @@
 /**
  * print_numbers - Prints numbers, followed by a new line.
  * @separator: The string to be printed between numbers.
- * @n: The numbers of integers passed to the function.
- * @...: A variable number of strings to be printed.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of integers to be printed.
  *
- * Return: if separator is NULL, don't print it.
+ * Description: This function takes a variable number of integers
+ * and prints them separated by the given string. If the separator
+ * is `NULL`, it is not printed.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
-va_list ap;
-int num;
-va_start(ap, n);
-for (i = 0; i < n; i++)
-{
-num = va_arg(ap, int);
-printf("%d", num);
-if (i < n - 1 && separator != NULL)
-printf("%s", separator);
+	unsigned int i; /* Index for iterating through the arguments */
+	va_list ap;     /* List to hold the variable arguments */
+	int num;        /* Variable to hold each integer */
+
+	/* Initialize the va_list to access the variable arguments */
+	va_start(ap, n);
+
+	/* Loop through all the integers passed to the function */
+	for (i = 0; i < n; i++)
+	{
+		/* Retrieve the next integer from the argument list */
+		num = va_arg(ap, int);
+
+		/* Print the integer */
+		printf("%d", num);
+
+		/* Print the separator if not NULL and not the last number */
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	/* Print a newline at the end */
+	printf("\n");
+
+	/* Clean up the va_list */
+	va_end(ap);
 }
-printf("\n");
-va_end(ap);
-}
+
