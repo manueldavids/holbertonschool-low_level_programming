fa2344537c1e4b5fb1492cba2be2f7d84d6f4995
voltamonkey <9461@holbertonstudents.com>
2024-12-06 15:59:52 -0400

    UpdatingMainc

--- variadic_functions/0-main.c
+++ variadic_functions/0-main.c
@@ -2,17 +2,28 @@
 #include "variadic_functions.h"
 
 /**
- * main - check the code
+ * main - Entry point to test variadic functions.
  *
- * Return: Always 0.
+ * This program demonstrates the use of the `sum_them_all` function
+ * by passing a variable number of arguments to calculate their sum.
+ *
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-    int sum;
+	int sum;
 
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
-    return (0);
+	/* Call sum_them_all with 2 arguments and store the result */
+	sum = sum_them_all(2, 98, 1024);
+	/* Print the result of the first sum */
+	printf("%d\n", sum);
+
+	/* Call sum_them_all with 4 arguments and store the result */
+	sum = sum_them_all(4, 98, 1024, 402, -1024);
+	/* Print the result of the second sum */
+	printf("%d\n", sum);
+
+	/* Indicate successful completion of the program */
+	return (0);
 }
+
