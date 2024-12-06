7b9827f654302581d41d1f698f20f98c83d607be
voltamonkey <9461@holbertonstudents.com>
2024-12-06 16:07:10 -0400

    SumUpdate

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,23 +3,34 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its parameters.
+ * sum_them_all - Calculates the sum of all its parameters.
  * @n: The number of parameters passed to the function.
- * @...: A variable number of parameters to calculate the sum of.
+ * @...: A variable number of arguments to calculate the sum of.
  *
- * Return: 0, if n == 0.
+ * Description: This function uses variadic arguments to calculate the
+ * sum of all its parameters. If no parameters are provided (`n == 0`),
+ * the function returns 0.
+ *
+ * Return: The sum of all parameters. If `n == 0`, returns 0.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
-int sum = 0;
-va_list ap;
-va_start(ap, n);
-for (i = 0; i < n; i++)
-{
-	sum += va_arg(ap, int);
-}
-va_end(ap);
-return (sum);
+	unsigned int i; /* Index for iterating through arguments */
+	int sum = 0;    /* Variable to store the sum of the parameters */
+	va_list ap;     /* Variable to hold the list of variadic arguments */
+
+	/* Initialize the va_list with the last fixed parameter */
+	va_start(ap, n);
+
+	/* Iterate through the arguments and calculate their sum */
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int); /* Add the next argument to the sum */
+	}
+
+	/* Clean up the va_list after use */
+	va_end(ap);
+
+	/* Return the calculated sum */
+	return (sum);
 }
