0e28db5ac3f66fd1827f2615db4892d75c388681
voltamonkey <9461@holbertonstudents.com>
2024-12-06 16:20:47 -0400

    1main

--- variadic_functions/1-main.c
+++ variadic_functions/1-main.c
@@ -1,12 +1,20 @@
 #include "variadic_functions.h"
 
 /**
- * main - check the code
+ * main - Entry point to test the print_numbers function.
  *
- * Return: Always 0.
+ * Description: This program demonstrates the use of the `print_numbers`
+ * function by passing a variable number of integers along with a separator
+ * to print them in a formatted way.
+ *
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
-    return (0);
+	/* Call print_numbers with a separator, count, and list of integers */
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+
+	/* Indicate successful completion of the program */
+	return (0);
 }
+
