355415e8272bdb9d2909138f17408abc01143bcc
voltamonkey <9461@holbertonstudents.com>
2024-12-10 11:17:53 -0400

    Copy content from one file to another with error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,51 +1,83 @@
 #include "main.h"
+#define BUFFER_SIZE 1024
+
+void close_file(int fd);
+void error_exit(int code, const char *message, const char *filename);
 
 /**
-* main - program that copies the content of a file to another file
-* @argc: num argument
-* @argv: string argument
-* Return: 0
-*/
-
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: 0 on success, exits with specific codes on failure.
+ */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	int num1 = 1024, num2 = 0;
-	char buf[1024];
+	int file_from, file_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		error_exit(97, "Usage: cp file_from file_to", NULL);
+
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-			| S_IRGRP | S_IWGRP | S_IROTH);
+		error_exit(98, "Error: Can't read from file", argv[1]);
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file_from), exit(99);
+		close_file(file_from);
+		error_exit(99, "Error: Can't write to", argv[2]);
 	}
-	while (num1 == 1024)
+
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		num1 = read(file_from, buf, 1024);
-		if (num1 == -1)
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+			close_file(file_from);
+			close_file(file_to);
+			error_exit(99, "Error: Can't write to", argv[2]);
 		}
-		num2 = write(file_to, buf, num1);
-		if (num2 < num1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	if (bytes_read == -1)
+	{
+		close_file(file_from);
+		close_file(file_to);
+		error_exit(98, "Error: Can't read from file", argv[1]);
 	}
 
-	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-
-	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-
+	close_file(file_from);
+	close_file(file_to);
 	return (0);
 }
+
+/**
+ * close_file - Closes a file descriptor and handles errors.
+ * @fd: The file descriptor to close.
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * error_exit - Prints an error message and exits with a code.
+ * @code: Exit code.
+ * @message: Error message to print.
+ * @filename: Filename related to the error (can be NULL).
+ */
+void error_exit(int code, const char *message, const char *filename)
+{
+	if (filename)
+		dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	else
+		dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
+
